dfdf32c85fec9662e03a98617c3bc685b74a962c
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-04 09:55:13 -0400

    Primer commit

---
+++ src/main/java/com/example/oneononedemotwo/OneononeDemoTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneononedemotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneononeDemoTwoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OneononeDemoTwoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/oneononedemotwo/OneononeDemoTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneononedemotwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneononeDemoTwoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
