5e459356773843b2f09adc0d0599a0a8e925c7d1
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-04 10:16:14 -0400

    Creadas entidades/tablas User y Profile

---
+++ src/main/java/com/example/oneononedemotwo/entity/Profile.java
@@ -0,0 +1,70 @@
+package com.example.oneononedemotwo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profile")
+public class Profile {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "phone_number")
+  private String phoneNumber;
+
+  @Column(name = "address")
+  private String address;
+
+  @OneToOne(mappedBy = "profile")
+  private User user;
+
+  public Profile() {
+  }
+
+  public Profile(Long id, String phoneNumber, String address,
+      User user) {
+    this.id = id;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+    this.user = user;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+}
